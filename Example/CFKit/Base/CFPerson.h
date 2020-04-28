f3d5927cf37ef5e372e911b9b2c6ef1ca2b521e6
cc <chengcheng@blued.com>
2020-04-28 11:41:01 +0800

    [描述]: 	1.添加Person文件 [模块]: [说明]:

---
+++ Example/CFKit/Base/CFPerson.h
@@ -0,0 +1,17 @@
+//
+//  CFPerson.h
+//  CFKit_Example
+//
+//  Created by cc on 2020/4/28.
+//  Copyright © 2020 Guhuotaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CFPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/CFKit/Base/CFPerson.m
@@ -0,0 +1,13 @@
+//
+//  CFPerson.m
+//  CFKit_Example
+//
+//  Created by cc on 2020/4/28.
+//  Copyright © 2020 Guhuotaren. All rights reserved.
+//
+
+#import "CFPerson.h"
+
+@implementation CFPerson
+
+@end
