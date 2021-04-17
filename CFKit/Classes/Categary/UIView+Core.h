23ca5035c3e45dbe1cc8a1ff3bb76f2a71bb17d0
cc <chengcheng@blued.com>
2021-04-17 17:13:42 +0800

    [描述]: 	1.添加圆角 [模块]: [说明]:

--- CFKit/Classes/Categary/UIView+Core.h
+++ CFKit/Classes/Categary/UIView+Core.h
@@ -59,6 +59,27 @@ NS_ASSUME_NONNULL_BEGIN
 //扩大点击区域
 - (void)cc_extendHitTestSizeByWidth:(CGFloat)width height:(CGFloat)height;
 
+
+
+#pragma mark - 设置部分圆角
+/**
+ *  设置部分圆角(绝对布局)
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
+ */
+- (void)cc_addRoundedCorners:(UIRectCorner)corners
+                   withRadii:(CGSize)radii;
+/**
+ *  设置部分圆角(相对布局)
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
+ *  @param rect    需要设置的圆角view的rect
+ */
+- (void)cc_addRoundedCorners:(UIRectCorner)corners
+                withRadii:(CGSize)radii
+                    viewRect:(CGRect)rect;
 @end
 
 NS_ASSUME_NONNULL_END
